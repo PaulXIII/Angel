bdb84050ad09840488d98081ef77c1951fa74d31
KessoPavel <pavel.kesso@gmail.com>
2019-11-22 23:34:30 +0300

    Add transaction from LoginFragment to AdvertisementFragmentList

--- app/src/main/java/com/androidacademy/angel/login/LoginFragment.kt
+++ app/src/main/java/com/androidacademy/angel/login/LoginFragment.kt
@@ -12,6 +12,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.androidacademy.angel.MainActivity
 import com.androidacademy.angel.R
+import com.androidacademy.angel.advertisementListPackage.AdvertisementFragmentList
 import com.androidacademy.angel.databinding.LoginFragmentBinding
 import com.androidacademy.angel.network.Repository
 import com.androidacademy.angel.prefs
@@ -42,11 +43,17 @@ class LoginFragment: Fragment() {
         viewModel.logInEvent.observe(this, Observer {isSuccess ->
             var string = ""
             if(isSuccess){
-                string = "Успех"
+                fragmentManager?.let {
+                    val fragment = AdvertisementFragmentList()
+                    it
+                        .beginTransaction()
+                        .replace(R.id.base_fragment_layout, fragment)
+                        .commit()
+                }
             }else{
                 string = getString(R.string.log_in_error)
+                Toast.makeText(activity, string, Toast.LENGTH_LONG).show()
             }
-            Toast.makeText(activity, string, Toast.LENGTH_LONG).show()
 
         })
 
