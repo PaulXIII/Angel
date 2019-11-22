2cc3db71b796ce13f90647eef061d0b0c821bb8e
KessoPavel <pavel.kesso@gmail.com>
2019-11-22 23:06:39 +0300

    Implements NewPersonFragment

--- app/src/main/java/com/androidacademy/angel/adding/NewPersonFragment.kt
+++ app/src/main/java/com/androidacademy/angel/adding/NewPersonFragment.kt
@@ -1,7 +1,51 @@
 package com.androidacademy.angel.adding
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.androidacademy.angel.R
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.add_new_fragment.*
 
 class NewPersonFragment : Fragment(){
+    companion object {
+        fun newInstance(): Fragment {
+            return NewPersonFragment()
+        }
+    }
 
+    private lateinit var viewModel: NewPersonViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.add_new_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(NewPersonViewModel::class.java)
+        viewModel.photoUrl.observe(this, Observer { url -> loadImage(url) })
+
+        publish.setOnClickListener{
+            viewModel.publish(title= title.text.toString(), description= description.text.toString())
+        }
+
+        photo_from_gallery.setOnClickListener{
+            viewModel.loadPhotoFromGallery()
+        }
+
+        take_photo.setOnClickListener{
+            viewModel.takePhoto()
+        }
+    }
+
+    fun loadImage(url: String?) {
+        Glide.with(this).load(url).placeholder(R.drawable.angel_logo).into(photo)
+    }
 }
--- app/src/main/java/com/androidacademy/angel/adding/NewPersonViewModel.kt
+++ app/src/main/java/com/androidacademy/angel/adding/NewPersonViewModel.kt
@@ -1,4 +1,21 @@
 package com.androidacademy.angel.adding
 
-class NewPersonViewModel {
+import android.text.Editable
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NewPersonViewModel: ViewModel() {
+    var photoUrl: MutableLiveData<String> = MutableLiveData()
+
+    fun publish(title: String, description: String){
+
+    }
+
+    fun takePhoto(){
+
+    }
+
+    fun loadPhotoFromGallery(){
+
+    }
 }
