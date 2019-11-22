91d7a56845e5ddefe34441b4b9a130663dafb328
VladimirMihalyuk <37588348+VladimirMihalyuk@users.noreply.github.com>
2019-11-22 18:02:43 +0300

    Added methods for regstration and login

--- app/src/main/java/com/androidacademy/angel/login/LoginFragment.kt
+++ app/src/main/java/com/androidacademy/angel/login/LoginFragment.kt
@@ -1,12 +1,14 @@
 package com.androidacademy.angel.login
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.androidacademy.angel.MainActivity
 import com.androidacademy.angel.R
+import com.androidacademy.angel.network.Repository
 import kotlinx.android.synthetic.main.login_fragment.view.*
 
 class LoginFragment: Fragment() {
@@ -17,8 +19,15 @@ class LoginFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.login_fragment, container, false)
 
+
         view.registration.setOnClickListener {
             (activity as MainActivity).globalViewModel.openRegistration()
+        }
+
+        view.log_in.setOnClickListener {
+            Repository.signIn(view.email.text.toString(), view.password.text.toString()){
+                isSuccessful -> Log.d("WTF", "$isSuccessful")
+            }
         }
 
 
--- app/src/main/java/com/androidacademy/angel/registration/RegistrationFragment.kt
+++ app/src/main/java/com/androidacademy/angel/registration/RegistrationFragment.kt
@@ -2,13 +2,15 @@ package com.androidacademy.angel.registration
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.androidacademy.angel.MainActivity
 
 import com.androidacademy.angel.R
+import com.androidacademy.angel.network.Repository
+import kotlinx.android.synthetic.main.login_fragment.view.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,10 +21,14 @@ class RegistrationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_registration, container, false)
+        val view = inflater.inflate(R.layout.registration_fragment, container, false)
 
 
-
+        view.registration.setOnClickListener {
+            Repository.signUp(view.email.text.toString(), view.password.text.toString()){
+                    isSuccessful -> Log.d("WTF", "$isSuccessful")
+            }
+        }
         return view
     }
 
