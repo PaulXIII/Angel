134ece35a7e661d818cdb56ee701a59054ed64d7
KessoPavel <pavel.kesso@gmail.com>
2019-11-23 07:47:43 +0300

    add share button

--- app/src/main/java/com/androidacademy/angel/details/DetailsFragment.kt
+++ app/src/main/java/com/androidacademy/angel/details/DetailsFragment.kt
@@ -1,16 +1,16 @@
 package com.androidacademy.angel.details
 
+import android.content.Intent
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 
 import com.androidacademy.angel.R
 import com.androidacademy.angel.data.AdvertModel
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.details_fragment.*
+
 
 class DetailsFragment : Fragment() {
     companion object {
@@ -33,6 +33,11 @@ class DetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.details_fragment, container, false)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -63,4 +68,24 @@ class DetailsFragment : Fragment() {
         viewModel = ViewModelProviders.of(this).get(DetailsViewModel::class.java)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.dwtails_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.share -> {
+                val sendIntent: Intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, "${advertModel?.url}\n${advertModel?.title}\n${advertModel?.description}")
+                    type = "text/plain"
+                }
+
+                val shareIntent = Intent.createChooser(sendIntent, null)
+                startActivity(shareIntent)
+            }
+        }
+        return true
+    }
 }
